144d915c708998d4fe5b9a0eb83e73f45b7d7cb6
Julio <IceXPR@users.noreply.github.com>
2023-06-10 00:08:49 -0400

    on ready

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-(function() {
+$(document).ready(function() {
     // Any initialization or loading logic for the variable
     $("#message").text("Hello, World!");
     // Rest of your code...
-  })();
+  });
